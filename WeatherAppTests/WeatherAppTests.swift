322f2e91a9e0e01b085d8036428e73e60afa6b9b
Mohamed Mernissi <mohamedmernissi@live.com>
2020-06-26 12:37:56 +0100

    Updates yml

--- WeatherAppTests/WeatherAppTests.swift
+++ WeatherAppTests/WeatherAppTests.swift
@@ -34,7 +34,7 @@ class WeatherAppTests: XCTestCase {
 
                 if let viewModel = self.presenter?.createViewModelFromModel(weatherModel: weatherModel){
                     //XCTAssertEqual(viewModel.city, "casablanca")
-                    XCTAssertEqual(viewModel.weather?.count, 8)
+                    XCTAssertEqual(viewModel.weather?.count, 5)
                     expectation.fulfill()
                 }
                 else{
