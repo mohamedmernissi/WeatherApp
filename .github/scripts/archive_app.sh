dcaaf34bb32f6548ac015693fa48559cec1fc922
Mohamed Mernissi <mohamedmernissi@live.com>
2020-07-01 13:13:56 +0100

    deploy

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 xcodebuild  -workspace WeatherApp.xcworkspace \
             -scheme "WeatherApp" \
             -sdk iphonesimulator \
-            -destination "platform=iOS Simulator,name=iPhone 11"
+            -destination "platform=iOS Simulator,name=iPhone 11" \
             -configuration AppStoreDistribution \
             -archivePath $PWD/build/WeatherApp.xcarchive \
             clean archive | xcpretty
