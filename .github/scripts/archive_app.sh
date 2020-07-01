3916ea7914777e7ee18da30ecc08a5b79d8eb075
Mohamed Mernissi <mohamedmernissi@live.com>
2020-07-01 11:23:28 +0100

    deploy

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -5,7 +5,6 @@ set -eo pipefail
 xcodebuild -workspace WeatherApp.xcworkspace \
             -scheme WeatherApp \
             -sdk iphoneos \
-            -allowProvisioningUpdates
             -configuration AppStoreDistribution \
             -archivePath $PWD/build/WeatherApp.xcarchive \
             clean archive | xcpretty
