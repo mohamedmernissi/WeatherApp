616f5f7ee25faf7887a87b94212b5a8ab221ce93
Mohamed Mernissi <mohamedmernissi@live.com>
2020-07-08 14:24:39 +0100

    deploy

---
+++ .github/scripts/import_provisioning.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Decrypt the files
+# --batch to prevent interactive command --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output secrets/Certificates.p12 secrets/Certificates.p12.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output secrets/WeatherApp_Distribution.mobileprovision secrets/WeatherApp_Distribution.mobileprovision.gpg
+
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+
+echo "List profiles"
+ls ~/Library/MobileDevice/Provisioning\ Profiles/
+echo "Move profiles"
+cp secrets/*.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+echo "List profiles"
+ls ~/Library/MobileDevice/Provisioning\ Profiles/
+
+security create-keychain -p "" build.keychain
+security import secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_KEYS" -A
+
+security list-keychains -s ~/Library/Keychains/build.keychain
+security default-keychain -s ~/Library/Keychains/build.keychain
+security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
