a685b7004f231c073fb5c04b3136d61ac9776264
Mohamed Mernissi <mohamedmernissi@live.com>
2020-07-08 14:38:56 +0100

    deploy

--- .github/scripts/import_provisioning.sh
+++ .github/scripts/import_provisioning.sh
@@ -2,8 +2,8 @@
 
 # Decrypt the files
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/WeatherApp_Distribution.mobileprovision ./.github/secrets/WeatherApp_Distribution.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output /.github/secrets/Certificates.p12 /.github/secrets/Certificates.p12.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output /.github/secrets/WeatherApp_Distribution.mobileprovision /.github/secrets/WeatherApp_Distribution.mobileprovision.gpg
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
@@ -15,7 +15,7 @@ echo "List profiles"
 ls ~/Library/MobileDevice/Provisioning\ Profiles/
 
 security create-keychain -p "" build.keychain
-security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_KEYS" -A
+security import /.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_KEYS" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
